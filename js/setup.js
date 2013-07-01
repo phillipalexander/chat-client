8b9eb5d8fc7d4824cfd182b3dc877c10f86e2d7e
Hack Reactor Students <git@phillipalexander.io>
2013-07-01 11:51:34 -0700

    Formatted HTML.

--- js/setup.js
+++ js/setup.js
@@ -12,3 +12,19 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+$.ajax('https://api.parse.com/1/classes/messages', {
+  contentType: 'application/json',
+  success: function(data){
+    $('#main').append(data.results[0].username);
+    _.each(data.results, function(value){
+      console.log('yes');
+      if (value.username){
+        $('#tweetStream').append("<tr><td><strong>@" + value.username + "</strong></td><td>" + value.text + "</td></tr>" /* <td><small class='muted'>"  + value.created_at.humane + "</small></td>"*/ );
+      }
+    });
+  },
+  error: function(data) {
+    console.log('Ajax request failed');
+  }
+});
